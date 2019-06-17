cad0c48797fa24eb0437286acf81f7417180eaa0
DanielAShurmer <thatblackshadowcat@outlook.com>
2019-06-17 10:37:49 +0100

    Initalization Commit

---
+++ Day 1/MathScripts.js
@@ -0,0 +1,19 @@
+function adder(InputA, InputB){
+    console.log(parseInt(InputA) + parseInt(InputB));
+}
+
+function subtracter(InputA, InputB){
+    console.log(parseInt(InputA) - parseInt(InputB));
+}
+
+function multiplier(InputA, InputB){
+    console.log(parseInt(InputA) * parseInt(InputB));
+}
+
+function divider(InputA, InputB){
+    console.log(parseInt(InputA) / parseInt(InputB));
+}
+
+function moduluser(InputA, InputB){
+    console.log(parseInt(InputA) % parseInt(InputB));
+}
